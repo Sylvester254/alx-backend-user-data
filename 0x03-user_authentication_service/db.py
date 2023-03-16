c483b3478d3d957a128b72e9a32fac4da82b0d06
Sylvester254 <slynganga59@gmail.com>
2023-03-16 22:03:59 +0300

    db edit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,31 +32,48 @@ class DB:
         return self.__session
     
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """
+        Add a new user to the database
+        Args:
+            email (str): The email address of the user
+            hashed_password (str): The hashed password of the user
+        Returns:
+            User: The newly created user
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
-    
+
     def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database based on input arguments
         """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound("No user found with the given arguments")
-            return user
-        except InvalidRequestError as e:
-            self._session.rollback()
-            raise e
+        Find a user by the given criteria
+        Args:
+            **kwargs: The criteria to search for
+        Returns:
+            User: The found user
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound("No user found")
+        return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user's attributes in the database
         """
-        user = self.find_user_by(id=user_id)
-        for attr, value in kwargs.items():
-            if not hasattr(user, attr):
-                raise ValueError(f"{attr} is not a valid attribute for User")
-            setattr(user, attr, value)
-        self._session.commit()
+        Update the given user
+        Args:
+            user_id (int): The id of the user to update
+            **kwargs: The fields to update
+        Returns:
+            None
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise InvalidRequestError
+            self._session.commit()
+        except (NoResultFound, InvalidRequestError, ValueError):
+            raise ValueError
